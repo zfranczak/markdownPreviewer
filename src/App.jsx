e4da212ac803712a1e67a455cefdf429f675d236
Zak Franczak <zakfranczak@gmail.com>
2023-07-12 10:48:32 -0500

    Added marked.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Editor from './components/Editor';
 import Header from './components/Header';
 import Previewer from './components/Previewer';
+import './components/marked';
 
 const App = () => {
   const [text, setText] = useState('');
