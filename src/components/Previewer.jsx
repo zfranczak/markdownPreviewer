416b37a1d8297b919fe40e6f4a192944042a0c82
Zak Franczak <zakfranczak@gmail.com>
2023-07-18 10:24:38 -0500

    got rid of all console errors

--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { marked } from 'marked';
+import { marked } from 'marked'; // Import the 'marked' library
+
+marked.setOptions({
+  breaks: true,
+  mangle: false,
+  headerIds: false,
+});
 
 const Previewer = ({ text }) => {
+  const html = marked(text);
+
   return (
     <div className='container outline'>
       <div className='toolbar'>Previewer</div>
-      <div
-        id='preview'
-        dangerouslySetInnerHTML={{ __html: marked(text) }}
-      ></div>
+      <div id='preview' dangerouslySetInnerHTML={{ __html: html }}></div>
     </div>
   );
 };
