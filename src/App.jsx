3e9f99b39b227eadd8d44bcd74182b565248d947
Zak Franczak <zakfranczak@gmail.com>
2023-07-18 09:38:06 -0500

    added initialText

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,32 @@ import Header from './components/Header';
 import Previewer from './components/Previewer';
 
 const App = () => {
-  const [text, setText] = useState('');
+  const initialText = `# Markdown Previewer
+
+  ## Sub Header
+  
+  This is a [link](https://www.example.com).
+  
+  Inline code: \`const message = 'Hello, World!';\`
+  
+  \`\`\`
+  // Code block
+  function add(a, b) {
+    return a + b;
+  }
+  \`\`\`
+  
+  - List item 1
+  - List item 2
+  
+  > This is a blockquote.
+  
+  ![Image](https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg)  
+  
+  **Bolded text**
+  `;
+
+  const [text, setText] = useState(initialText);
 
   const handleTextChange = (newText) => {
     setText(newText);
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../styles/editor.css';
 
-const Editor = ({ onTextChange }) => {
-  const handleChange = (event) => {
-    const newText = event.target.value;
+const Editor = ({ onTextChange, text }) => {
+  const handleChange = (e) => {
+    const newText = e.target.value;
     onTextChange(newText);
   };
 
   return (
     <div className='container outline'>
       <div className='toolbar'>Editor</div>
-      <textarea id='editor' className='editorTextBox' onChange={handleChange} />
+      <textarea
+        id='editor'
+        className='editorTextBox'
+        onChange={handleChange}
+        value={text}
+      />
     </div>
   );
 };
