6639cedc5e62b9739d53793394ceda52c6cea888
Zak Franczak <zakfranczak@gmail.com>
2023-07-18 13:53:54 -0500

    Github issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,50 @@ import Header from './components/Header';
 import Previewer from './components/Previewer';
 
 const App = () => {
-  const initialText = `# Markdown Previewer
+  const initialText = `# Welcome to my React Markdown Previewer!
 
-  ## Sub Header
+  ## This is a sub-heading...
+  ### And here\'s some other cool stuff:
   
-  This is a [link](https://www.example.com).
-  
-  Inline code: \`const message = 'Hello, World!';\`
+  Heres some code, \`<div></div>\`, between 2 backticks.
   
   \`\`\`
-  // Code block
-  function add(a, b) {
-    return a + b;
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == \'\`\`\`\' && lastLine == \'\`\`\`\') {
+      return multiLineCode;
+    }
   }
   \`\`\`
   
-  - List item 1
-  - List item 2
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
   
-  > This is a blockquote.
+  There\'s also [links](https://www.zacharyfranczak.com), and
+  > Block Quotes!
   
-  ![Image](https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg)  
+  And if you want to get really crazy, even tables:
   
-  **Bolded text**
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![Doggo](https://images.pexels.com/photos/3487734/pexels-photo-3487734.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)
+  
   `;
 
   const [text, setText] = useState(initialText);
@@ -38,7 +59,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
-      <Editor onTextChange={handleTextChange} />
+      <Editor onTextChange={handleTextChange} text={text} />
       <Previewer text={text} />
     </div>
   );
